ef4b8bd6488544e551199156b2db0efd7a53102a
icepy <xiangwenwe@foxmail.com>
2017-10-15 01:10:23 +0800

    修改alias路径

--- build/alias.js
+++ build/alias.js
@@ -4,9 +4,18 @@ function absolute (str) {
   return path.resolve(__dirname, '..', str)
 }
 
+// console.log({
+//   'dingtalk-sdk-exec': absolute('src/package/dingtalk-sdk-exec/index.js'),
+//   'dingtalk-sdk-ship': absolute('src/package/dingtalk-sdk-ship/index.js'),
+//   'shared': absolute('src/shared'),
+//   'core': absolute('src/core'),
+//   'dingtalk-javascript-utility': absolute('../dingtalk-javascript-utility/src/local.index.js')
+// })
+
 module.exports = {
   'dingtalk-sdk-exec': absolute('src/package/dingtalk-sdk-exec/index.js'),
   'dingtalk-sdk-ship': absolute('src/package/dingtalk-sdk-ship/index.js'),
   'shared': absolute('src/shared'),
-  'core': absolute('src/core')
+  'core': absolute('src/core'),
+  'dingtalk-javascript-utility': absolute('../dingtalk-javascript-utility/src/local.index.js')
 };
